4a3403d19c3f442bcbae5d19c0c61156cf057a35
ANANDHU S <71482562+anandhu-eng@users.noreply.github.com>
2020-12-06 19:19:15 +0530

    published the first version

---
+++ youtube vedio downloader/try.py
@@ -0,0 +1,34 @@
+#youtube vedio downloader
+from tkinter import *
+from pytube import YouTube
+from tkinter import messagebox
+
+
+#function for downloading the youtube vedio
+def download():
+	vedio_url=url.get()
+	print(vedio_url)
+	youtube=YouTube(vedio_url)
+	youtube.title
+	vedio=youtube.streams.first()
+	print(vedio)
+	vedio.download()
+	messagebox.showmessage("SUCCESS!","Vedio successfully downloaded!:)")
+
+#main window
+win=Tk()
+win.title("YouTube video Downloader")
+#creating labels
+label1=Label(win, text="YouTube Video Downloader",fg="red",font=("calibri",10)).grid(sticky=N,padx=100,row=0)
+label2=Label(win,text="Please enter the URL of the video to download:",fg="black",font=("calibri",10,"bold")).grid(sticky=N,row=1,pady=2)
+url=StringVar()
+#Entry
+#should split the entry column bcoz  In python when you do a().b(), the result of the expression is whatever b() returns, therefore Entry(...).grid(...) will return None.
+url=Entry(win,textvariable=url,width=50)
+url.grid(sticky=N,row=2,pady=4)
+
+#button
+button1=Button(win,text="Download video",command=download).grid(sticky=N,row=3,pady=20)
+
+
+win.mainloop()
